c3a37002ea91d738f1acbd697c8f69cc0e9d1aaf
Kentaro Tanaka <kentanaka101@gmail.com>
2019-05-25 12:35:52 +0900

    Add spec for NotifyAPI::Client

---
+++ spec/line/notify_api/client_spec.cr
@@ -0,0 +1,44 @@
+require "../../spec_helper"
+require "webmock"
+
+module Line::NotifyAPI
+  describe Client do
+    it "token" do
+      client = Client.new("token")
+
+      boundary = MIME::Multipart.generate_boundary
+
+      body = convert_multipart_body(<<-BODY)
+      --#{boundary}
+      Content-Disposition: form-data; name="message"
+      
+      Hello, World!
+      --#{boundary}--
+      BODY
+
+      headers = {"Content-Type" => "multipart/form-data; boundary=\"#{boundary}\"", "Authorization" => "Bearer token"}
+
+      WebMock.stub(:post, "https://notify-api.line.me/api/notify")
+        .with(body: body, headers: headers)
+        .to_return(body: "")
+
+      client.notify("Hello, World!", boundary: boundary)
+
+      body = convert_multipart_body(<<-BODY)
+      --#{boundary}
+      Content-Disposition: form-data; name="message"
+      
+      Hello, again!
+      --#{boundary}--
+      BODY
+
+      headers = {"Content-Type" => "multipart/form-data; boundary=\"#{boundary}\"", "Authorization" => "Bearer token"}
+
+      WebMock.stub(:post, "https://notify-api.line.me/api/notify")
+        .with(body: body, headers: headers)
+        .to_return(body: "")
+
+      client.notify("Hello, again!", boundary: boundary)
+    end
+  end
+end
